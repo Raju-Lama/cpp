aa618496cc166a5cced9e1b07dcb0f8b170e6564
Raju Lama <rajulamarainon@gmail.com>
2024-01-11 23:25:32 +0545

    conditional compilation to see ifthere is a preprocesor derective defined or not

---
+++ conditional compilation/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#define MAN
+//contitional compilation, if the condiction is met, only the codes are compiled.
+int main()
+{
+#ifdef MAN//since the MAN is defined, it compiles
+    std::cout << "This is compiled and output\n";
+#endif
+
+#ifdef WOMAN
+    std::cout << "This text is never compiled haha,, this is just like a comment box" ;
+#endif
+
+#ifndef WOMAN
+    std::cout << "This is compiled as this is not defined which is exactly opposite of #ifdef.\n";
+#endif // WOMAN
+
+#if 0//codes in betweens are never compiled, its like a comment for multi lines inside multiles.
+    std::cout << "This is never compiled or printed.";
+    /*
+    *these are comments inside comment.
+    *
+    */
+#endif // 0
+    return 0;
+}
