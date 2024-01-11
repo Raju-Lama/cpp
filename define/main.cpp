e2ac267f4ccb3bbf5842c3adb53e98346afdb1b0
Raju Lama <rajulamarainon@gmail.com>
2024-01-11 22:50:41 +0545

    changes

--- define/main.cpp
+++ define/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 
-#define My_Name "Raju"// not a statement, but, this is used to name an identifier/ like creating data type in 'c/
-
+//this is outdated and is not a good practice now
+#define MY_NAME "Raju"// not a statement, but, this is used to name an identifier/ like creating data type in 'c/
+//preprocessor directive are  without semicolon;
 void foo()
 {
-#define Last_Name "Good"//this doesn't make any sense, as define is preprocesor section taht doesn't understand the syntax of c++
+#define LAST_NAME "Good"//this doesn't make any sense, as define is preprocesor section taht doesn't understand the syntax of c++
 //it can be compiled and run
     std::cout << "This is another function.\n";
 }
 
 int main()
 {
-    std::cout << My_Name << '\n';
-    std::cout << Last_Name << '\n';
+    std::cout << MY_NAME << '\n';
+    std::cout << LAST_NAME << '\n';
     foo();
     return 0;
 }
