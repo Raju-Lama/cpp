bfa9290948cf1d19901271f4963306d88b3f92a3
Raju Lama <rajulamarainon@gmail.com>
2024-01-13 22:58:15 +0545

    good practices to design a program

---
+++ best practices to desing a program/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//the best practice is to outline the main function. moreover, you have to think a little bit at first
+//about the program  you want to create, the inputs, users, outputs that are needed.. etc.
+int main()
+{
+    //to create a simple calculator
+    //outlining the main functions as
+    /*
+    *   userInput();
+    *   operator();
+    *   userSecondInput();
+    *   result();
+    *   print();
+    */
+
+    //going from simple is important to lead a better non-trivial program in the long term.
+    return 0;
+}
