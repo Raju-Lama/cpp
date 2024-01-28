07122b923a764534e06c14f4e538dab6832a16ef
Raju Lama <rajulamarainon@gmail.com>
2024-01-29 00:17:14 +0545

    updates to type conversion

--- implicit type conversion/main.cpp
+++ implicit type conversion/main.cpp
@@ -2,12 +2,18 @@
 //implicit type conversion
 //converting one fundamental data type to another, by compiler without asking for,
 // the compiler automatically converts the type in this regard,
-int enterSomething(double x)
+double enterSomething(double x)//accepts double
 {
     return x;
 }
+void print(double x)
+{
+    std::cout << x << '\n' ;//should print 5.0;
+}
 int main()
 {
-    std::cout << enterSomething(5);
+    std::cout << enterSomething(5) << '\n' ;//we are inputting an integer type;//should print 5.0
+    int x{5};
+    print(x);//calling with integer value of x;
     return 0;
 }
