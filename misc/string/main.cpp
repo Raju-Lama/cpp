dcc74cdabbc38e1f5c031271961fe152f5edc627
Raju Lama <rajulamarainon@gmail.com>
2024-02-19 00:52:51 +0545

    changes

--- misc/string/main.cpp
+++ misc/string/main.cpp
@@ -8,5 +8,10 @@ int main()
     std::cout << "My name is " << name << '\n' ;
     name = "fulTanky";
     std::cout << "My name is " << name << '\n' ;
+    std::cout << "Enter your full name :: ";
+    std::getline(std::cin >> std::ws, name);
+    //std::ws tells cin to ignore the white space...
+    //std::ws is an input manipulator, used in each getline()
+    std::cout << "Your name is " << name << '\n' ;
     return 0;
 }
