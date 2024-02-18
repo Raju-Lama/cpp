a92ed7047add4599cf72e8986f37001870649e49
Raju Lama <rajulamarainon@gmail.com>
2024-02-19 00:26:31 +0545

    string

---
+++ misc/string/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>//for std::string
+int main()
+{
+    std::string name{"Raju"};
+    std::cout << "My name is " << name << '\n';
+    name = "Happy";
+    std::cout << "My name is " << name << '\n' ;
+    name = "fulTanky";
+    std::cout << "My name is " << name << '\n' ;
+    return 0;
+}
