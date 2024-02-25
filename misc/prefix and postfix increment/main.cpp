86ec7de429c5fc4117307ebb3b17af7de5dfb304
Raju Lama <rajulamarainon@gmail.com>
2024-02-25 15:54:04 +0545

    update

--- misc/prefix and postfix increment/main.cpp
+++ misc/prefix and postfix increment/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
+//always use prefixed one which is best for performance.
 int main()
 {
     int x{0};
     int y{x++};
     std::cout << y << std::endl;//prints 0
-    std::cout << y << std::endl;//prints 0
+    std::cout << y << std::endl;//prints 0y
     ++x;
     std::cout << x;//prints 2
     return 0;
