c3ef41d7f6bbbb9e18e0d3db0c81a8cd48deca1a
Raju Lama <rajulamarainon@gmail.com>
2024-04-26 00:11:25 +0545

    friend non member function

---
+++ class/friend non member function/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+// accessing class member using non member function with friend keyword
+
+class Connect
+{
+private:
+    int m_id{2};
+
+public:
+
+    friend void Friendship(const Connect& connect);//forward declaration
+
+};
+
+void Friendship(const Connect& connect)
+{
+    std::cout << connect.m_id;
+}
+
+int main()
+{
+    Connect mem{};
+    Friendship(mem);//calling non member function.
+    return 0;
+}
