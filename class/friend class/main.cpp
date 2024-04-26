be38b321e5d95be6e65fec9bc51e87c21a3533d9
Raju Lama <rajulamarainon@gmail.com>
2024-04-27 00:44:10 +0545

    friend class

---
+++ class/friend class/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+//friend class
+class Dude
+{
+private:
+    int m_id{};
+
+public:
+    Dude(int id)
+        :m_id{id}
+    {
+    }
+
+    friend class Gal;//dude is her friend now, but, dude is not friend of her unless gal puts him in her list
+};
+
+class Gal
+{
+private:
+    int m_no{};
+
+public:
+    Gal(int num)
+        :m_no {num}
+    {
+    }
+        void print(const Dude& dude)
+        {
+            std::cout << dude::m_id << '\n' ;
+        }
+};
+int main()
+{
+    Dude boy{2};
+    Gal girl{3};
+
+    Gal::print(boy);
+
+    return 0;
+}
