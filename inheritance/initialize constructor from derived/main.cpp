13b66e0fab3b3ae56529fff09994e8a7b2b8bbaa
Raju Lama <rajulamarainon@gmail.com>
2024-05-19 00:14:49 +0545

    update

--- inheritance/initialize constructor from derived/main.cpp
+++ inheritance/initialize constructor from derived/main.cpp
@@ -18,10 +18,20 @@ class Derived : public Base
 private :
     double m_marks{};
 
+public :
+    Derived (double marks=0.0, int id=0)
+        : m_marks{marks},
+        Base {id}//to initialize the private member of the base class/parent class from child class
+    {
+    }
+
+    double getMarks() const { return m_marks; }
 };
 
 int main()
 {
-
+    Derived derived (44.5, 5);
+    std::cout << "BaseId :: " << derived.getBase() << '\n';
+    std::cout << "Marks :: " << derived.getMarks() << '\n';
     return 0;
 }
