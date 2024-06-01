c5c61f2e932ac30977482a66a1399bc34bb7ff27
Raju <rajulamarainon@gmail.com>
2024-06-01 19:28:43 +0545

    return by reference

---
+++ return by reference/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//return by reference with static variable, otherwise, it becomes dangling
+#include <string>
+
+const std::string& returnByReference()
+{
+    static const std::string staticValue {"Hello "};//it gets destroyed only after the end of the program
+    return staticValue;
+}
+
+int main()
+{
+    std::cout << returnByReference() << "world!" << '\n';
+
+    return 0;
+}
