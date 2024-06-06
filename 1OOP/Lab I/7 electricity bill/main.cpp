95ed67c9063a31052a94e0b885efdf581e531f7a
rajukaji <rajulamarainon@gmail.com>
2024-06-07 01:23:55 +0545

    finalizing

--- 1OOP/Lab I/7 electricity bill/main.cpp
+++ 1OOP/Lab I/7 electricity bill/main.cpp
@@ -13,6 +13,7 @@ number of units consumed and print out the charges with name.
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 int main()
 {
@@ -24,7 +25,26 @@ int main()
     float unit{};
     std::cin >> unit;
 
-    std::cout << "Electricity bills ::" << std::endl;
+    float charge{0};
+    if (unit <= 100) charge += unit * 0.6;//60P per unit, converted into ruppee
+
+    if (unit <= 300) charge += 100 * 0.6 + (unit - 100) * 0.8;
+
+    if (unit > 300) charge += 100 * 0.6 + 200 * 0.8 + (unit - 300) * 0.9;
+
+    float total {charge + 50};
+
+    if (total > 300) total += total * 0.15;//15% surcharge
+
+    std::cout << "\nElectricity bills ::" << std::endl << '\n' ;
+
+    std::cout << std::left;
+    std::cout << std::setw(20) << "Name" << std::setw(20) << "Units consumed";
+    std::cout << std::setw(20) << "Charge" << std::endl;
+
+    std::cout << std::setw(20) << name << std::setw(20) << unit;
+    std::cout << std::setw(20) << total << std::endl;
+
 
     return 0;
 }
