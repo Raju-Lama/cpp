c3939c3a3541773ddaa18981a1cbd359922797b1
rajukaji <rajulamarainon@gmail.com>
2024-06-12 01:01:12 +0545

    class done fine

--- 1OOP/Lab I/7 with class/main.cpp
+++ 1OOP/Lab I/7 with class/main.cpp
@@ -14,53 +14,78 @@ number of units consumed and print out the charges with name.
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cstddef>//for std::size_t, as length of dynamically allocated array has type size_t
 
 class Users
 {
 public:
-    std::string m_name;
+    std::string m_name{};
     float m_unit{};
+    float m_charge{};
+    float m_total{};
+
 };
 
 int main()
 {
-    Users* us{new Users};//dynamic memory allocation
-
     std::cout << "Enter the number of customers :: ";
     int n;
     std::cin >> n;
+    //this leaves a newline at the buffer
+
+    Users* user { new Users[n]{} };//dynamic memory allocation
 
     for(int i = 0; i < n; i++)
     {
-        std::cout << "Enter Full Name :: ";
-        std::getline (std::cin, us[i].m_name);
+        std::cout << "User " << i + 1 << " :: \n" ;
+        std::cout << "  Full name :: " ;
+        //fist need to ignore the new line \n buffer first, otherwise, string is not inputted.
+        //using std::cin.ignore(1, '\n')
+        //or we can simple use ignore white space, getline(std::cin >> std::ws, str), but, there is still an error
+        std::cin.ignore();//clear the buffer, default ignore(1,'\n')
+        std::getline(std::cin, user[i].m_name);
 
-        std::cout << "Enter the units consumed :: ";
-        std::cin >> us[i].m_unit;
+        std::cout << "  Units consumed :: ";
+        std::cin >> user[i].m_unit;
+        std::cout << std::endl;
 
     }
-    /*
 
-    float charge{0};
-    if (unit <= 100) charge += unit * 0.6;//60P per unit, converted into ruppee
+    for(int i = 0; i < n; i++)
+    {
+        if (user[i].m_unit <= 100)
+            user[i].m_charge += user[i].m_unit * 0.6;//60P per unit, converted into ruppee
 
-    if (unit <= 300) charge += 100 * 0.6 + (unit - 100) * 0.8;
+        if (user[i].m_unit <= 300)
+            user[i].m_charge += 100 * 0.6 + (user[i].m_unit - 100) * 0.8;
 
-    if (unit > 300) charge += 100 * 0.6 + 200 * 0.8 + (unit - 300) * 0.9;
+        if (user[i].m_unit > 300)
+            user[i].m_charge += 100 * 0.6 + 200 * 0.8 + (user[i].m_unit - 300) * 0.9;
 
-    float total {charge + 50};//minumum 50 ruppees added
+        user[i].m_total = user[i].m_charge + 50;//minumum 50 ruppees added
 
-    if (total > 300) total += total * 0.15;//15% surcharge
+        if (user[i].m_total > 300) user[i].m_total += user[i].m_total * 0.15;//15% surcharge
+    }
+
 
     std::cout << "\nElectricity bills ::" << std::endl << '\n' ;
 
     std::cout << std::left;
-    std::cout << std::setw(20) << "Name" << std::setw(20) << "Units consumed";
+
+    std::cout << std::setw(20) << "Name";
+    std::cout << std::setw(20) << "Units consumed";
     std::cout << std::setw(20) << "Charge (Rs)" << std::endl;
 
-    std::cout << std::setw(20) << name << std::setw(20) << unit;
-    std::cout << std::setw(20) << total << std::endl;
-*/
 
+    for(int i = 0; i < n; i++)
+    {
+        std::cout << std::left;
+        std::cout << std::setw(20) << user[i].m_name;
+        std::cout << std::setw(20) << user[i].m_unit;
+        std::cout << std::setw(20) << user[i].m_total << std::endl;
+    }
+
+    delete[] user;//dynamically deleting array
+    user = nullptr;//optional, since its going out of scope
     return 0;
 }
